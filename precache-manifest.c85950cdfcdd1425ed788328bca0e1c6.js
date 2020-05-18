00ee0d1065dd852af467f896060e47fe74961010
Gabriel Machado <gadu.machado@gmail.com>
2020-05-18 18:50:24 -0300

    Updates

---
+++ precache-manifest.c85950cdfcdd1425ed788328bca0e1c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf133b2f37ce71b08632fe88bed43fb",
+    "url": "/animations3d/index.html"
+  },
+  {
+    "revision": "1f887344a75e9139e5e6",
+    "url": "/animations3d/static/css/main.1119e6f6.chunk.css"
+  },
+  {
+    "revision": "ba364f3c87c08415a48b",
+    "url": "/animations3d/static/js/2.322c811a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/animations3d/static/js/2.322c811a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f887344a75e9139e5e6",
+    "url": "/animations3d/static/js/main.cc22facf.chunk.js"
+  },
+  {
+    "revision": "1c79ed0b58d8d7aac730",
+    "url": "/animations3d/static/js/runtime-main.c3f890a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animations3d/static/media/logo.5d5d9eef.svg"
+  }
+]);
